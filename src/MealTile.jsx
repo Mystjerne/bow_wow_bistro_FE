1fc53e9512283858c36a4d42f3c8245fb02adc5f
Mystjerne <danaliewqie@gmail.com>
2024-03-12 01:27:35 +0800

    1. Basic project setup (env files, installing packages, routes) 2. Made basic image carousel component 3. Made OrderPage and MealTile components

---
+++ src/MealTile.jsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { useState, onClick } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
+import EditNoteRoundedIcon from "@mui/icons-material/EditNoteRounded";
+
+export default function MealTile({
+  meal_img_path,
+  meal_name,
+  meal_description,
+}) {
+  const addMealToUserCart = () => {
+    console.log("Add button clicked!");
+  };
+
+  return (
+    <Card className="card" sx={{ maxWidth: 345, padding: 2, margin: 3 }}>
+      <CardMedia sx={{ height: 140 }} image={meal_img_path} title="Meal" />
+      {/* //The p stands for padding. How to make the padding white? */}
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          textAlign={"center"}
+        >
+          {meal_name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {meal_description}
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        {/* Make the buttons circular */}
+        <Button size="small">
+          <EditNoteRoundedIcon />
+        </Button>
+
+        <Button size="small" onClick={addMealToUserCart}>
+          <AddCircleOutlineRoundedIcon />
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/OrderPage.jsx
+++ src/OrderPage.jsx
@@ -1,13 +1,64 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Container, Link } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  Grid,
+} from "@mui/material";
+import MealTile from "./MealTile";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+// import.meta.env.BACKEND_MEAL_URL;
+
 /*
 <AppBar position="static"></AppBar>
 */
 
 function OrderPage() {
+  //Make a get request to the backend for all the meals.
+
+  const [mealData, setMealData] = useState([]);
+
+  useEffect(() => {
+    console.log(mealData);
+  }, [mealData]);
+
+  useEffect(() => {
+    //get the specific job listing data.
+    //also get all applications associated with that job listing.
+    console.log(import.meta.env.VITE_SOME_BACKEND_MEAL_URL);
+    axios
+      .get(`${import.meta.env.VITE_SOME_BACKEND_MEAL_URL}`)
+      .then((response) => {
+        const mealdata = response.data;
+
+        console.log(response.data);
+        setMealData(mealdata);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  var allmealtiles = mealData.map((meal, index) => (
+    <Grid key={index}>
+      <MealTile
+        meal_name={meal.mealName}
+        meal_description={meal.mealDescription}
+        meal_img_path={meal.mealPhoto}
+      />
+    </Grid>
+  ));
+
   return (
     <div>
       <h1>
+        <Grid container spacing={0}>
+          {allmealtiles}
+        </Grid>
         {/* This is the order page, where people can pick meals and put them in
         their carts.
         //Put orders in a grid. each order is a box that has three things in it: a photo, a button to add it to the cart, and an edit button. */}
